142bf0cb8db41d96221a4f0cdf7073e7d93b46c2
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-08-04 10:37:25 +0900

    Time: 5414 ms (21.97%), Space: 13.9 MB (90.49%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        First = [i for i, digit in enumerate(digits) if digit != 0]
+        Second = list(range(0, len(digits)))
+        Last = [i for i, digit in enumerate(digits) if digit %2 == 0]
+        Answer = set()
+        for i in First:
+            for j in Second:
+                for k in Last:
+                    if i != j and i != k and j != k:
+                        Answer.add(digits[i]*100 + digits[j]*10 + digits[k])
+        Answer = list(Answer)
+        Answer.sort()
+        return Answer
