1f0a45369893c459b5d6f00b38feb579f86fed01
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-08-05 10:07:15 +0900

    Time: 1246 ms (30.61%), Space: 14.9 MB (77.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,20 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # taget이 nums의 요소 일부를 더한 것과 같아지면 -> 해당 요소의 nums 인덱스 번호를 리스트 형태로 반환한다
+    def twoSum(self, nums: List[int], target: int) -> List[int]:    
         
+        # enumerate 풀이방법
+        pairs = [target - num for num in nums] # nums = [2,7,11,15] -> pairs = [7,2,-2,-6]
+        
+        for i , pair in enumerate(pairs): #pairs의 각각의 key와 value -> (0,7),(1,2),(2,-2),(3,-6)
+            
+            if pair in nums and i != nums.index(pair): #pair가 nums의 요소이고, key값이 nums에서 pair의 인덱스값과                                                                      다르면
+                return [i, nums.index(pair)]
+        
+
+        
+        '''
+        2중 for문 풀이
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if target == (nums[i]+nums[j]):
                     return [i,j]
+        '''
