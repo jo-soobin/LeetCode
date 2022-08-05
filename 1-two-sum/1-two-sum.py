cf9c71fedb1d9886d30fbe42180d31c52015865b
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-08-05 11:38:11 +0900

    Time: 81 ms (79.47%), Space: 15 MB (77.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:    
+               
+        # Two-pointer 풀이방법
+        nums_sorted = sorted(nums)
+        i, j = 0 , len(nums)-1
+        while i<j:
+            left = nums_sorted[i]
+            right = nums_sorted[j]
+            diff = left+right-target
+            
+            if diff <0:
+                i+=1
+            elif diff>0:
+                j-=1
+            else:
+                break
         
+        if left != right:
+            return nums.index(left), nums.index(right)
+        else:
+            return [i for i, num in enumerate(nums) if num==left]
+
+        '''
         # hash table 풀이방법
         hashtable = dict()
         for i, value in enumerate(nums): #(0,2),(1,7),(2,11),(3,15)
@@ -11,6 +32,7 @@ class Solution:
         for i, pair in enumerate(pairs): #(0,7),(1,2),(2,-2),(3,-6)
             if pair in hashtable and i != hashtable[pair]: # 7,2,-2,-6 in hashtable and                                                                                          0,1,2,3 != hashtable[7.2.-2.-6]
                 return [i, hashtable[pair]]
+        '''
         
         '''
         # enumerate 풀이방법
