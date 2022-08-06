4f2f9ec105d3cb2763916c514c7de4e433348fe1
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-08-06 15:46:33 +0900

    Time: 36 ms (92.43%), Space: 13.9 MB (75.84%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,8 +1,8 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:   
         for i in t:
-            if(i not in s or s.count(i)!=t.count(i)):
-                 return i
+            if i not in s or s.count(i) != t.count(i):
+                return i
     
 '''class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
