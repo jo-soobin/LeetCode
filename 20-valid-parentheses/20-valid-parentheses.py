c7dffb5363692536e994d845e0f9cef4cb222d8c
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-08-12 10:44:45 +0900

    Time: 65 ms (14.41%), Space: 13.9 MB (25.50%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        inputlst=list(s)
+        lst = []
+        for i in inputlst:
+            if i in ['(','{','[']:
+                lst.append(i)
+            else:
+                if len(lst) == 0:
+                    return False
+                if lst[len(lst)-1] == '(' and i == ')':
+                    lst.pop()
+                elif lst[len(lst)-1] == '{' and i =='}':
+                    lst.pop()
+                elif lst[len(lst)-1] == '[' and i ==']':
+                    lst.pop()
+                else:
+                    return False
+            
+        if len(lst)>0:
+            return False
+        return True
+    
