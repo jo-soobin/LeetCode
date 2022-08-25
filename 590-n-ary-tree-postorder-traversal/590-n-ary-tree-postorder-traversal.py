f93ebf0fd761c607285e868e3b5e2447e76c75e6
Soobin, Jo <90607293+jo-soobin@users.noreply.github.com>
2022-08-25 14:14:34 +0900

    Time: 67 ms (72.08%), Space: 16.3 MB (17.54%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.py
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.py
@@ -5,27 +5,16 @@ class Node:
         self.val = val
         self.children = children
 """
-
-
 class Solution:
-    def DFS(self, Node):
-        if not Node:
-            return
-        for child in Node.children:
-            self.DFS(child)
-        self.Answer.append(Node.val)
     def postorder(self, root: 'Node') -> List[int]:
-        self.Answer = []
+        self.answer = []
         self.DFS(root)
-        return self.Answer
-    '''def DFS(self, Node):
+        return self.answer
+    
+    def DFS(self, Node):
         if not Node:
             return
         for child in Node.children:
             self.DFS(child)
         self.answer.append(Node.val)
         
-    def postorder(self, root: 'Node') -> List[int]:
-        answer = []
-        self.DFS(root)
-        return answer'''
